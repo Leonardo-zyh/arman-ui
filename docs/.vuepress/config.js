100ea492cb399b7f13f2d10ce8c5ad2e55167584
arman <td19941113@gmail.com>
2019-02-13 11:33:58 +0800

    favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,30 +1,33 @@
-module.exports = {
-    title: 'arman-ui',
-    description: '一个简单好用的UI框架',
-    themeConfig: {
-        nav: [
-            { text: '主页', link: '/' },
-            { text: '安装', link: '/guide/install' },
-            { text: '快速上手', link: '/guide/start' },
-            { text: 'GitHub', link: 'https://github.com/Arman19941113/arman-ui' },
+module.exports={
+    title:'arman-ui',
+    description:'一个简单好用的UI框架',
+    head:[
+        ['link',{rel:'shortcut icon',type:"image/x-icon",href:`./favicon.ico`}]
+    ],
+    themeConfig:{
+        nav:[
+            {text:'主页',link:'/'},
+            {text:'安装',link:'/guide/install'},
+            {text:'快速上手',link:'/guide/start'},
+            {text:'GitHub',link:'https://github.com/Arman19941113/arman-ui'},
         ],
-        sidebar: [
+        sidebar:[
             {
-                title: '开发指南',
-                path: '/guide/',
-                collapsable: false,
-                sidebarDepth: 1,
-                children: [
+                title:'开发指南',
+                path:'/guide/',
+                collapsable:false,
+                sidebarDepth:1,
+                children:[
                     '/guide/install',
                     '/guide/start'
                 ]
             },
             {
-                title: '组件',
-                path: '/component/',
-                collapsable: false,
-                sidebarDepth: 1,
-                children: [
+                title:'组件',
+                path:'/component/',
+                collapsable:false,
+                sidebarDepth:1,
+                children:[
                     '/component/button',
                     '/component/input',
                     '/component/tabs',
