40507f768d4da0a0bd29b944e823c18abc64ae02
arman <td19941113@gmail.com>
2019-02-13 11:17:55 +0800

    collapse组件

---
+++ docs/.vuepress/components/ACollapse1.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<a-collapse>
+			<a-collapse-item title="我">I</a-collapse-item>
+			<a-collapse-item title="爱" active>LOVE</a-collapse-item>
+			<a-collapse-item title="你">YOU</a-collapse-item>
+		</a-collapse>
+	</div>
+</template>
+<script>
+    import ACollapse from '../../../src/components/ACollapse'
+    import ACollapseItem from '../../../src/components/ACollapseItem'
+    export default {
+        components:{
+            ACollapse,ACollapseItem
+        }
+    }
+</script>
+<style>
+	*, *::before, *::after{box-sizing:border-box;}
+</style>
--- docs/component/collapse.md
+++ docs/component/collapse.md
@@ -1,1 +1,14 @@
 # Collapse 折叠面板
+**基础用法**
+
+<ClientOnly>
+<ACollapse1/>
+</ClientOnly>
+
+```html
+<a-collapse>
+    <a-collapse-item title="我">I</a-collapse-item>
+    <a-collapse-item title="爱" active>LOVE</a-collapse-item>
+    <a-collapse-item title="你">YOU</a-collapse-item>
+</a-collapse>
+```
